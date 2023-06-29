5c161a8dd003fe174fd32b9feb2216d1fe445038
Rob Nunez <res1hlmv@gmail.com>
2023-06-28 19:55:23 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+/*   cloud {
+    organization = "RNunezDevTeam"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  } */
 
   required_providers {
     aws = {
